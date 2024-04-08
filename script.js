cc4141144f2860e04778d0000c2addf19cc6fa62
Dominik Komar <46048127+dominikkomar-github@users.noreply.github.com>
2024-04-08 14:41:32 +0100

    History Feature Implemented
    - History feature works as intended now
    - CSS Changes to the history section

--- script.js
+++ script.js
@@ -37,73 +37,38 @@ const songs = [
     },
 
     {
-        id: '6',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/6.jpg"
+      id: '6',
+      songName: `NIGHTMARE <br>
+      <h5>SXMPRA</h5>`,
+      image: "img/2.jpg"
     },
 
     {
-        id: '7',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/7.jpg"
+      id: '7',
+      songName: `Rock Star <br>
+      <h5>Post Malone</h5>`,
+      image: "img/1.jpg",
     },
 
     {
-        id: '8',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/8.jpg"
+      id: '8',
+      songName: `9MM <br>
+      <h5>Memphis Cult</h5>`,
+      image: "img/4.jpg",
     },
 
     {
-        id: '9',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/9.jpg"
+      id: '9',
+      songName: `Tot Musica <br>
+      <h5>Ado</h5>`,
+      image: "img/5.jpg",
     },
 
     {
-        id: '10',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/10.jpg"
-    },
-
-    {
-        id: '11',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/11.jpg"
-    },
-
-    {
-        id: '11',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/11.jpg"
-    },
-
-    {
-        id: '11',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/11.jpg"
-    },
-
-    {
-        id: '11',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/11.jpg"
-    },
-
-    {
-        id: '11',
-        songName: `Rock Star <br>
-        <h5>Post Malone</h5>`,
-        image: "img/11.jpg"
+      id: '10',
+      songName: `Bécane <br>
+      <h5>Yamê</h5>`,
+      image: "img/3.jpg",
     },
 
 ];
@@ -258,4 +223,70 @@ volumeSlider.addEventListener('change', (event) => {
 
   // Set the audio element's volume property
   music.volume = newVolume;
+});
+
+
+const playlistHistory = [];
+
+// Function to add played song to the playlist history
+function addToPlaylistHistory(song) {
+  playlistHistory.unshift(song); // Add the song to the beginning of the array
+  if (playlistHistory.length > 3) {
+    playlistHistory.pop(); // Remove the oldest song if the array length exceeds 4
+  }
+  updatePlaylistHistoryUI(); // Update the UI to reflect the changes
+}
+
+// Function to update the UI for the playlist history
+function updatePlaylistHistoryUI() {
+  const historyItems = document.querySelectorAll('.playlist .historyItem');
+
+  playlistHistory.forEach((song, index) => {
+    const historyItem = historyItems[index];
+    const titleElement = historyItem.querySelector('span');
+    titleElement.innerHTML = `
+      <span>${song.songName}</span>
+      <img src="${song.image}" alt="${song.songName} image">
+    `;
+  });
+}
+
+// Add event listener to update playlist history when a song is clicked
+Array.from(document.querySelectorAll('.main-music .songItem')).forEach((element, i) => {
+  element.addEventListener('click', () => {
+    const selectedSong = {
+      songName: songs[i].songName,
+      image: songs[i].image // Ensure the song object includes the image property
+    };
+    addToPlaylistHistory(selectedSong); // Add the clicked song to the playlist history
+  });
+});
+
+// Event listener for when a song ends to update the next song and playlist history
+music.addEventListener('ended', () => {
+  if (index < songs.length - 1) {
+    index++;
+    const nextSongItem = songs[index];
+    const nextSongAudio = nextSongItem.querySelector('audio');
+
+    // Update the audio source and play
+    music.src = nextSongAudio.src;
+    music.play();
+
+    const nextSong = {
+      songName: nextSongItem.querySelector('p').textContent,
+      artist: nextSongItem.querySelector('audio').getAttribute('src'), // Extract artist information from the audio source
+      image: nextSongItem.image // Access the image for the next song
+    };
+    addToPlaylistHistory(nextSong); // Add the next song to the playlist history
+
+    // Assuming you have a UI element with a class "currentSong" to display the current song's details
+    const currentSongElement = document.querySelector('.currentSong');
+    currentSongElement.innerHTML = `
+      <span>${nextSong.songName}</span>
+      <img src="${nextSong.image}" alt="${nextSong.songName} image">
+    `;
+  } else {
+    console.error("Invalid song index:", index);
+  }
 });
